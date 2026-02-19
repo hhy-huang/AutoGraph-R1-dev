3b4d5a0e5b366b248598a3a66b313aeebca96869
hhy-huang <haoyuhuang@link.cuhk.edu.hk>
2026-02-20 02:22:18 +0800

    refinement inference scripts

---
+++ scripts/autograph-r1/eval/gr_refine_bench_naivecons.sh
@@ -0,0 +1,1 @@
+python benchmark/autograph/benchmarking_text_refiner.py --model_name /data/haoyuhuang/model/models--Qwen--Qwen2.5-3B-Instruct/snapshots/aa8e72537993ba99e69dfaafa59ed015b17504d1 --refine
